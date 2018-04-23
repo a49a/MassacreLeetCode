80463216ab7b5d08a58338426e236462baa58903
deadwind4 <rsl4@foxmail.com>
2018-04-23 21:33:13 +0800

    完成二叉树后序和中序遍历

---
+++ src/94.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def __init__(self):
+        self.List = []
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        self.traversal(root)
+        return self.List
+    def traversal(self, root):
+        if root == None:
+            return
+        else:
+            self.traversal(root.left)
+            self.List.append(root.val)
+            self.traversal(root.right)
