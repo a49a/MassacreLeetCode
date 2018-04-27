c00f71220f7aeaef4c570e088744fb7ed5d77800
deadwind4 <rsl4@foxmail.com>
2018-04-27 21:20:50 +0800

    完成110一部分

--- src/110.py
+++ src/110.py
@@ -4,11 +4,33 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+import sys
 class Solution:
+    def __init__(self):
+        self.min_depth = sys.maxsize
+        self.max_depth = 0
     def isBalanced(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        
+        if root is None:
+            return True
+        [_, isBalanced] = self.gen_depth(root, 0)
+        return isBalanced
+    def gen_depth(self, root, cur_depth):
+        cur_depth += 1
+        if root.left is None and root.right is None:
+            return [cur_depth, True]
+        left_depth = 0
+        right_depth = 0
+        if root.left is not None:
+            [left_depth, isBalanced] = self.gen_depth(root.left, cur_depth)
+        if root.right is not None:
+            [right_depth, isBalanced] = self.gen_depth(root.right, cur_depth)
+        if isBalanced is False:
+            return [cur_depth, False]
+        if abs(left_depth - right_depth) <= 1:
+            return [cur_depth + max(left_depth, right_depth), True]
+        else:
+            return [cur_depth, False]
